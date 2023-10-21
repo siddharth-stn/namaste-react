62014a4daf183a956ec246d5cad152e72bd9e99b
Siddharth Pande <sidarjunstn.sp@gmail.com>
2023-10-21 16:04:07 +0530

    Class Component

--- src/components/UserClass.jsx
+++ src/components/UserClass.jsx
@@ -1,15 +1,10 @@
 import React from "react";
-
 class UserClass extends React.Component {
-  constructor(props) {
-    super(props);
-    console.log(props);
-  }
   render() {
     return (
       <div className="user-card">
-        <h2>Name: </h2>
-        <h3>Location: </h3>
+        <h2>Name: {name}</h2>
+        <h3>Location: Dehradun</h3>
         <h4>Contact: @akshaymarch7</h4>
       </div>
     );

cb1a8ceafa9a4b82985a56624b35c41bbbcbbedb
Siddharth Pande <sidarjunstn.sp@gmail.com>
2023-10-21 16:25:57 +0530

    Class Based Components Use of Super

--- src/components/UserClass.jsx
+++ src/components/UserClass.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 class UserClass extends React.Component {
+  constructor(props) {
+    // super(props);
+    super();
+    console.log(props);
+    console.log(this.props);
+  }
   render() {
+    // const { name } = this.props;
     return (
       <div className="user-card">
-        <h2>Name: {name}</h2>
-        <h3>Location: Dehradun</h3>
+        <h2>Name: {this.props.name}</h2>
+        <h3>Location: {this.props.location}</h3>
         <h4>Contact: @akshaymarch7</h4>
       </div>
     );
